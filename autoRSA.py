2127f2883df42e836aacb736021911cae23d0f47
deolu <adesolaadeoluwa@gmail.com>
2024-08-29 05:03:44 +0100

    move the database connection setup to the bot's on_ready event

--- autoRSA.py
+++ autoRSA.py
@@ -51,23 +51,9 @@ except Exception as e:
 # Initialize .env file
 load_dotenv()
 
-# Database connection
-conn = sqlite3.connect("rsa_bot_users.db")
-cursor = conn.cursor()
-
-# Create a table for storing credentials if it doesn't exist
-cursor.execute(
-    """
-CREATE TABLE IF NOT EXISTS rsa_credentials (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id TEXT NOT NULL,
-    broker TEXT NOT NULL,
-    username TEXT NOT NULL,
-    password TEXT NOT NULL
-)
-"""
-)
-conn.commit()
+# Global database connection
+conn = None
+cursor = None
 
 # Global variables
 SUPPORTED_BROKERS = [
@@ -337,17 +323,36 @@ if __name__ == "__main__":
         # Bot event when bot is ready
         @bot.event
         async def on_ready():
+            global conn, cursor
             channel = bot.get_channel(DISCORD_CHANNEL)
             if channel is None:
                 print(
                     "ERROR: Invalid channel ID, please check your DISCORD_CHANNEL in your .env file and try again"
                 )
                 os._exit(1)  # Special exit code to restart docker container
+            # Database connection
+            conn = sqlite3.connect("rsa_bot_users.db")
+            cursor = conn.cursor()
+
+            # Create a table for storing credentials if it doesn't exist
+            cursor.execute(
+                """
+            CREATE TABLE IF NOT EXISTS rsa_credentials (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                user_id TEXT NOT NULL,
+                broker TEXT NOT NULL,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL
+            )
+            """
+            )
+            conn.commit()
             await channel.send("Discord bot is started...")
 
         @bot.event
         async def on_disconnect():
-            conn.close()
+            if conn:
+                conn.close()
 
         # Process the message only if it's from the specified channel
         @bot.event
