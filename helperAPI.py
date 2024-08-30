e6a53df19a6dbecf5882a7408254ed636f6f8bb6
deolu <adesolaadeoluwa@gmail.com>
2024-08-30 22:10:12 +0100

    explicitly specify web driver path

--- helperAPI.py
+++ helperAPI.py
@@ -516,10 +516,10 @@ def getDriver(DOCKER=False):
     # Init webdriver options
     try:
         options = webdriver.ChromeOptions()
+        options.binary_location = "/usr/bin/google-chrome"
         options.add_argument("start-maximized")
         options.add_experimental_option("excludeSwitches", ["enable-automation"])
         options.add_experimental_option("useAutomationExtension", False)
-        options.setBinary("/usr/bin/google-chrome");
         options.add_argument("--disable-blink-features=AutomationControlled")
         options.add_argument("--disable-extensions")
         options.add_argument("--disable-notifications")
